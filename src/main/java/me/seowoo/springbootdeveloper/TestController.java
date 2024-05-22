44d92a958e235759e471efa071e5b9191982247e
seowooNote <dkr0210@naver.com>
2024-05-22 13:40:41 +0900

    3일차 study

--- src/main/java/me/seowoo/springbootdeveloper/TestController.java
+++ src/main/java/me/seowoo/springbootdeveloper/TestController.java
@@ -1,12 +1,24 @@
 package me.seowoo.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+//    @GetMapping("/test")
+//    public String test() {
+//        return "Hello, world!";
+//    }
+
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello, world!";
+    public List<Member> getAllMember() {
+        List<Member> members = testService.getAllMemebers();
+        return members;
     }
 }
